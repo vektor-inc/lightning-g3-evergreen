aa8e8be3adf1ec1e1b30ec2aa7ba1caed124b32a
goutetsuguma <goutetsuguma@gmail.com>
2021-06-30 21:10:56 +0900

    add:googlefont読み込み追加

--- functions.php
+++ functions.php
@@ -1,8 +1,18 @@
 <?php
-
+// bodyクラスを追加
 add_filter(
     'body_class',
     function ( $classes ) {
         return array_merge( $classes, array( 'ltg3-s-evergreen' ) );
     }
+);
+
+// Google Fontを追加.
+add_action(
+    'wp_enqueue_scripts',
+    function () {
+        // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+        wp_enqueue_style( 'ltg3-s-evergreen-googlefonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap', array(), null );
+    },
+    12
 );

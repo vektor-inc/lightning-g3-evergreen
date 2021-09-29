843110aa5cab42a6a591d9ea39df7ebde0dd1b3b
goutetsuguma <goutetsuguma@gmail.com>
2021-09-29 13:45:27 +0900

    [ Change version ] 0.0.6

--- lightning-g3-evergreen.php
+++ lightning-g3-evergreen.php
@@ -7,7 +7,7 @@
  * Author URI:      https://vektor-inc.co.jp
  * Text Domain:     lightning-g3-evergreen
  * Domain Path:     /languages
- * Version:         0.0.5
+ * Version:         0.0.6
  *
  * @package         LIGHTNING_G3_EVERGREEN
  */
